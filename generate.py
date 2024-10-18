cf658868ebd1112d209de878b8f92b2dd61ccc5b
Matias Graña <matias.alejo@gmail.com>
2024-10-18 08:22:49 +0200

    add infinity points

--- generate.py
+++ generate.py
@@ -1,22 +1,41 @@
 import galois
 
 
+def p2f_index(gf, x, y) -> int:
+    order = gf.order
+    if y is None:
+        ix = int(x) if x is not None else order
+        return order**2 + ix
+    return int(x) + order * int(y)
+
+
 def generate_cards(gf):
     """ugly first version of cards generator.
     TODO:
         - typing
-        - infinity points
         - nice symbols
     """
     order = gf.order
     for a in range(order):
+        agf = gf(a)
         for b in range(order):
-            agf = gf(a)
             bgf = gf(b)
-            cards = [int(agf * gf(x) + bgf) for x in range(gf.order)]
-            print(f"card ({a},{b}) --> {cards}")
+            cards = [p2f_index(gf, x := gf(i), agf * x + bgf) for i in range(order)]
+            cards.append(p2f_index(gf, a, None))
+            yield cards
+        cards = [p2f_index(gf, a, gf(i)) for i in range(order)]
+        cards.append(p2f_index(gf, None, None))
+        yield cards
+    cards = [p2f_index(gf, gf(i), None) for i in range(order)]
+    cards.append(p2f_index(gf, None, None))
+    yield cards
 
 
-order = 7
+def print_cards(gf):
+    for cards in generate_cards(gf):
+        print(f"{cards}")
+
+
+order = 2
 GF = galois.GF(order)
-generate_cards(GF)
+print_cards(GF)
