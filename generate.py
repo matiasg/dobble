b99aa9b841a1802cbd79857655831b1bd40c5b15
Matias Graña <matias.alejo@gmail.com>
2024-10-18 22:25:04 +0200

    add typing and arg parser for order

--- generate.py
+++ generate.py
@@ -1,4 +1,7 @@
+from argparse import ArgumentParser
+
 import galois
+from galois._fields._meta import FieldArrayMeta
 
 
 def default_graph(i):
@@ -6,33 +9,38 @@ def default_graph(i):
     return chr(e0 + i)
 
 
-def p2f_index(gf, x, y) -> int:
+def p2f_index(
+    x: galois.FieldArray | None, y: galois.FieldArray | None, gf: FieldArrayMeta
+) -> int:
+    """`x` and `y` must be elements of the same Galois Field `gf` or None.
+    If `x` is None, then `y` must be None as well.
+    """
+    assert gf is not None
     order = gf.order
     if y is None:
         ix = int(x) if x is not None else order
         return order**2 + ix
+    assert x is not None, f"x is None but y == {y} != None"
     return int(x) + order * int(y)
 
 
-def generate_cards(gf):
-    """ugly first version of cards generator.
-    TODO:
-        - typing
-        - nice symbols
+def generate_cards(gf: FieldArrayMeta):
+    """
+    Generates cards over Galois field `gf`
     """
     order = gf.order
     for a in range(order):
         agf = gf(a)
         for b in range(order):
             bgf = gf(b)
-            cards = [p2f_index(gf, x := gf(i), agf * x + bgf) for i in range(order)]
-            cards.append(p2f_index(gf, a, None))
+            cards = [p2f_index(x := gf(i), agf * x + bgf, gf) for i in range(order)]
+            cards.append(p2f_index(gf(a), None, gf))
             yield cards
-        cards = [p2f_index(gf, a, gf(i)) for i in range(order)]
-        cards.append(p2f_index(gf, None, None))
+        cards = [p2f_index(gf(a), gf(i), gf) for i in range(order)]
+        cards.append(p2f_index(None, None, gf))
         yield cards
-    cards = [p2f_index(gf, gf(i), None) for i in range(order)]
-    cards.append(p2f_index(gf, None, None))
+    cards = [p2f_index(gf(i), None, gf) for i in range(order)]
+    cards.append(p2f_index(None, None, gf))
     yield cards
 
 
@@ -42,6 +50,22 @@ def print_cards(gf):
         print(f"{graphs}")
 
 
-order = 7
-GF = galois.GF(order)
-print_cards(GF)
+def main(args):
+    order = args.order
+    GF = galois.GF(order)
+    print_cards(GF)
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "order",
+        type=int,
+        help="order of the Galois field, which is 1 less than the number of drawing in each card",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
