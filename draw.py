c3cfef3c729f7a5f33e21204d0a8025d10e8ba04
Matias Graña <matias.alejo@gmail.com>
2024-10-21 08:51:13 +0200

    refactor positions in make_card

--- draw.py
+++ draw.py
@@ -1,31 +1,43 @@
+import logging
 import math
 from argparse import ArgumentParser
+from itertools import count
 from pathlib import Path
+from typing import Iterator
 
 from svgelements import SVG, Circle, Matrix, Point
 
 from generate import generate_cards
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def make_card(svgs, indices, end_size=300.0):
+
+def positions_generator(
+    N: int, sizes: float, end_size: float
+) -> Iterator[tuple[float, float]]:
+    theta = math.cos(2 * math.pi / N) + 1j * math.sin(2 * math.pi / N)
+    for i in range(N):
+        pos = theta**i * 0.8 * end_size
+        x = pos.real
+        y = pos.imag
+        yield (x - sizes / 2 + end_size, y - sizes / 2 + end_size)
+
+
+def make_card(svgs, indices, end_size=300.0) -> SVG:
     out_svg = SVG(width=2 * end_size, height=2 * end_size)
     N = len(indices)
-    theta = math.cos(2 * math.pi / N) + 1j * math.sin(2 * math.pi / N)
-    for i, idx in enumerate(indices):
+    sizes = end_size / math.sqrt(N)
+    for i, idx, (x, y) in zip(
+        count(), indices, positions_generator(N, sizes, end_size)
+    ):
         svg_file, svg = svgs[idx]
-        print(f"  using {svg_file} for card at pos {i}")
-        x = (theta**i).real
-        y = (theta**i).imag
+        logger.info(
+            "  using %s for card at pos %d: (x,y)=(%.3f, %.3f)", svg_file, i, x, y
+        )
         size = max(svg.width, svg.height)
         for e in svg.elements():
-            out_svg.append(
-                e
-                * Matrix.scale(end_size / size / N)
-                * Matrix.translate(
-                    end_size * (x * 0.8 + 1 - 1 / N / 2),
-                    end_size * (y * 0.8 + 1 - 1 / N / 2),
-                )
-            )
+            out_svg.append(e * Matrix.scale(sizes / size) * Matrix.translate(x, y))
     out_svg.append(
         Circle(
             center=Point(end_size, end_size),
@@ -43,7 +55,7 @@ def make_cards(svgs: list[tuple[Path, SVG]], order: int, outdir: Path):
     symbols = order**2 + order + 1
     assert len(svgs) >= symbols, f"need at least {symbols} svgs but got {len(svgs)}"
     for i, card in enumerate(generate_cards(order)):
-        print(f"doing card {i}")
+        logger.info("doing card %d", i)
         svg = make_card(svgs, card)
         svg.write_xml(outdir / f"card_{i}.svg")
 
